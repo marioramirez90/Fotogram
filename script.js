d918c5e8517b739280d5eb129c536c694d5bedd8
marioramirez90 <mario-ramirez@hotmail.de>
2026-01-06 11:35:20 +0100

    script:added img

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const myImage = [
-"img/"
+"img/img1.jpg",
+"img/img2.jpg",
+"img/img3.png",
+"img/img4.jpg",
+"img/img5.jpg",
+"img/img6.jpg",
+"img/img7.jpg",
+"img/img8.jpg",
+"img/img9.jpg",
+"img/img10.jpg",
+"img/img11.jpg",
+"img/img12.jpg",
 
-]
+
+];
+
+function loadMyImage(){
+    const myImageRef = document.getElementById('image');
+
+    for(let i = 1; i < myImage.length; i++){
+    myImageRef.innerHTML += `<img id="gallery${i}" class="gallery-image" src="${myImage[i]}" alt="myImage${i}">`;
+    }
+    }
