d6bd77265a20f1517d6f8dad9be302205d5553f3
marioramirez90 <mario-ramirez@hotmail.de>
2026-01-07 11:09:46 +0100

    added  fuction numbersOfImage

--- script.js
+++ script.js
@@ -14,27 +14,37 @@ const myImage = [
 
 
 ];
+let currentIndex = 0;
 
 function loadMyImage(){
     const myImageRef = document.getElementById('image');
 
     for(let i = 0; i < myImage.length; i++){
-    myImageRef.innerHTML += `<img onclick="openGalleryImage()" id="gallery${i}" class="gallery-image" src="${myImage[i]}" alt="myImage${i}">`;
+    myImageRef.innerHTML += `<img onclick="openGalleryImage(${i})" id="gallery${i}" class="gallery-image" src="${myImage[i]}" alt="myImage${i}">`;
     }
     }
 
 
     const dialogRef = document.getElementById("openGallery");
-    function openGalleryImage(){
+    function openGalleryImage(index){
+        currentIndex =index;
         dialogRef.showModal();
         dialogRef.classList.add("open-Image");
+        document.getElementById("openGalleryImage").innerHTML = `<img src="${myImage[index]}" class="open-gallery-image">`;
+        document.getElementById("numbersOfImages").textContent= `${currentIndex + 1}/${myImage.length}`;
+
+        let myImageName =myImage[index];
+        document.getElementById("ImageTitle").textContent=myImageName;
     }
+   
+
     function closeGalleryImage(){
         dialogRef.close();
         dialogRef.classList.remove("open-Image")
     }
 
 
+    
 
 
 
